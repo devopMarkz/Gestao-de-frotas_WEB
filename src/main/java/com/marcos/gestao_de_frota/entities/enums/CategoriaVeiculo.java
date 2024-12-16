5cab26a238727f8b3d2fbf6bdcf35f4735dba0ed
devopMarkz <marcosdev2002@gmail.com>
2024-12-16 17:22:05 -0300

    Criando enums referentes a status da viagem e categoria o veiculo

---
+++ src/main/java/com/marcos/gestao_de_frota/entities/enums/CategoriaVeiculo.java
@@ -0,0 +1,6 @@
+package com.marcos.gestao_de_frota.entities.enums;
+
+public enum CategoriaVeiculo {
+    ONIBUS,
+    CAMINHAO
+}
---
+++ src/main/java/com/marcos/gestao_de_frota/entities/enums/StatusViagem.java
@@ -0,0 +1,7 @@
+package com.marcos.gestao_de_frota.entities.enums;
+
+public enum StatusViagem {
+    EM_ANDAMENTO,
+    CONCLUIDA,
+    CANCELADA
+}
