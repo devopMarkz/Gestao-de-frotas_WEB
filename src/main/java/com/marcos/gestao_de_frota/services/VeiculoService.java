5aca6a90bd17107cb6d0d55b3b97adf2be358d7d
devopMarkz <marcosdev2002@gmail.com>
2024-12-18 09:34:03 -0300

    Definindo as implementações de VeiculoFactory dentro de um Map

--- src/main/java/com/marcos/gestao_de_frota/services/VeiculoService.java
+++ src/main/java/com/marcos/gestao_de_frota/services/VeiculoService.java
@@ -3,32 +3,52 @@ package com.marcos.gestao_de_frota.services;
 import com.marcos.gestao_de_frota.dto.veiculo.CreateVeiculoDto;
 import com.marcos.gestao_de_frota.dto.veiculo.VeiculoDto;
 import com.marcos.gestao_de_frota.entities.Caminhao;
+import com.marcos.gestao_de_frota.entities.Onibus;
+import com.marcos.gestao_de_frota.entities.Veiculo;
 import com.marcos.gestao_de_frota.factory.VeiculoFactory;
 import com.marcos.gestao_de_frota.repositories.VeiculoRepository;
+import com.marcos.gestao_de_frota.services.exceptions.VeiculoInvalidoException;
 import com.marcos.gestao_de_frota.utils.ConvertEntityToDto;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 public class VeiculoService {
 
     private VeiculoRepository veiculoRepository;
-    private VeiculoFactory veiculoFactory; // testando com caminhão
+    private Map<String, VeiculoFactory> factories;
 
-    public VeiculoService(VeiculoRepository veiculoRepository, @Qualifier("caminhaoFactory") VeiculoFactory veiculoFactory){
+    @Autowired
+    public VeiculoService(VeiculoRepository veiculoRepository, Map<String, VeiculoFactory> factories) {
         this.veiculoRepository = veiculoRepository;
-        this.veiculoFactory = veiculoFactory;
+        this.factories = factories;
     }
 
     @Transactional
-    public VeiculoDto insert(CreateVeiculoDto createVeiculoDto){
-        if(createVeiculoDto.getCategoriaVeiculo().equalsIgnoreCase("CAMINHAO")) {
-            Caminhao caminhao = (Caminhao) veiculoFactory.criarVeiculo(createVeiculoDto);
-            caminhao = veiculoRepository.save(caminhao);
-            return ConvertEntityToDto.convertToCaminhaoDto(caminhao);
+    public VeiculoDto insert(CreateVeiculoDto createVeiculoDto) {
+        // Recupera a fábrica com base na categoria do veículo
+        String tipo = createVeiculoDto.getCategoriaVeiculo().toLowerCase() + "Factory";
+        VeiculoFactory factory = factories.get(tipo);
+
+        if (factory == null) {
+            throw new VeiculoInvalidoException("Tipo de veículo inválido: " + createVeiculoDto.getCategoriaVeiculo());
         }
-        throw new IllegalArgumentException("Veículo inexistente.");
+
+        Veiculo veiculo = factory.criarVeiculo(createVeiculoDto);
+
+        veiculo = veiculoRepository.save(veiculo);
+
+        return convertToDto(veiculo);
+    }
+
+    private VeiculoDto convertToDto(Veiculo veiculo) {
+        return switch (veiculo.getCategoriaVeiculo().name()) {
+            case "CAMINHAO" -> ConvertEntityToDto.convertToCaminhaoDto((Caminhao) veiculo);
+            default -> throw new VeiculoInvalidoException("Tipo de veículo inválido.");
+        };
     }
 
 }
