3c6c26aae58d993152dafcb4fdb08a4d1aba7cf3
devopMarkz <marcosdev2002@gmail.com>
2024-12-20 07:55:50 -0300

    Validando o objeto aninhado dentro de CreateMotoristaForUsuario

--- src/main/java/com/marcos/gestao_de_frota/dto/usuario/CreateMotoristaForUsuarioDto.java
+++ src/main/java/com/marcos/gestao_de_frota/dto/usuario/CreateMotoristaForUsuarioDto.java
@@ -1,14 +1,17 @@
 package com.marcos.gestao_de_frota.dto.usuario;
 
 import com.marcos.gestao_de_frota.dto.motorista.CreateMotoristaDto;
-import com.marcos.gestao_de_frota.dto.motorista.MotoristaDto;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 public class CreateMotoristaForUsuarioDto {
 
     @Email(message = "Formato de e-mail inválido.")
     private String email;
 
+    @Valid
+    @NotNull(message = "O campo cadastroDeMotorista não pode estar nulo.")
     private CreateMotoristaDto cadastroDeMotorista;
 
     public CreateMotoristaForUsuarioDto(String email, CreateMotoristaDto cadastroDeMotorista) {
