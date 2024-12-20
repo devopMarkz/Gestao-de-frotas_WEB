5bf293a8b9cba68d0c75d9574a9686b488d291d6
devopMarkz <marcosdev2002@gmail.com>
2024-12-20 09:38:03 -0300

    Criando consulta personalizada para puxar os dados do motorista junto com o seu usuario

--- src/main/java/com/marcos/gestao_de_frota/repositories/UsuarioRepository.java
+++ src/main/java/com/marcos/gestao_de_frota/repositories/UsuarioRepository.java
@@ -2,11 +2,13 @@ package com.marcos.gestao_de_frota.repositories;
 
 import com.marcos.gestao_de_frota.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    @Query("SELECT obj FROM Usuario obj JOIN FETCH obj.motorista WHERE obj.email = :email AND obj.password = :password")
     Optional<Usuario> findByEmailAndPassword(String email, String password);
 
     Optional<Usuario> findByEmail(String email);
