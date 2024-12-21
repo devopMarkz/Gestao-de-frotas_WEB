031f026efebfd6d54a567b2b3168ad6b7bcf814a
devopMarkz <marcosdev2002@gmail.com>
2024-12-21 00:42:07 -0300

    Ajustando DTO

--- src/main/java/com/marcos/gestao_de_frota/dto/aluguel/AluguelDto.java
+++ src/main/java/com/marcos/gestao_de_frota/dto/aluguel/AluguelDto.java
@@ -8,28 +8,20 @@ import java.time.LocalDateTime;
 
 public class AluguelDto {
 
-    private MotoristaDto motorista;
-    private VeiculoDto veiculo;
     private LocalDateTime dataHoraInicio;
     private LocalDateTime dataHoraFim;
     private StatusAluguel statusAluguel;
     private Double valorAluguel;
+    private MotoristaDto motorista;
+    private VeiculoDto veiculo;
 
     public AluguelDto(MotoristaDto motorista, VeiculoDto veiculo, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim, StatusAluguel statusAluguel, Double valorAluguel) {
-        this.motorista = motorista;
-        this.veiculo = veiculo;
         this.dataHoraInicio = dataHoraInicio;
         this.dataHoraFim = dataHoraFim;
         this.statusAluguel = statusAluguel;
         this.valorAluguel = valorAluguel;
-    }
-
-    public MotoristaDto getMotorista() {
-        return motorista;
-    }
-
-    public VeiculoDto getVeiculo() {
-        return veiculo;
+        this.motorista = motorista;
+        this.veiculo = veiculo;
     }
 
     public LocalDateTime getDataHoraInicio() {
@@ -46,5 +38,13 @@ public class AluguelDto {
 
     public Double getValorAluguel() {
         return valorAluguel;
+    }
+
+    public MotoristaDto getMotorista() {
+        return motorista;
+    }
+
+    public VeiculoDto getVeiculo() {
+        return veiculo;
     }
 }
