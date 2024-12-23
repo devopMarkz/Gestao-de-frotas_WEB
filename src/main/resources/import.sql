843c7b43bfc6e163e395cb3c7a28edebc634acfa
devopMarkz <marcosdev2002@gmail.com>
2024-12-22 21:03:35 -0300

    Criando seeding na base de dados H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,31 @@
+-- Inserindo Caminhões
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'ABC1234', 'Scania', 'P360', 2020, 20000.0, 500.0, 'Caminhao');
+INSERT INTO tb_caminhao (veiculo_id, numero_de_eixos) VALUES (1, 3);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'DEF5678', 'Volvo', 'FH', 2019, 25000.0, 600.0, 'Caminhao');
+INSERT INTO tb_caminhao (veiculo_id, numero_de_eixos) VALUES (2, 4);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'GHI9101', 'Mercedes-Benz', 'Actros', 2021, 22000.0, 550.0, 'Caminhao');
+INSERT INTO tb_caminhao (veiculo_id, numero_de_eixos) VALUES (3, 5);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'JKL2345', 'MAN', 'TGX', 2018, 21000.0, 570.0, 'Caminhao');
+INSERT INTO tb_caminhao (veiculo_id, numero_de_eixos) VALUES (4, 3);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'MNO6789', 'Iveco', 'Stralis', 2022, 24000.0, 650.0, 'Caminhao');
+INSERT INTO tb_caminhao (veiculo_id, numero_de_eixos) VALUES (5, 6);
+
+-- Inserindo Ônibus
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'XYZ1234', 'Volvo', 'B7R', 2015, 45000.0, 300.0, 'Onibus');
+INSERT INTO tb_onibus (veiculo_id, numero_de_assentos) VALUES (6, 42);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'UVW5678', 'Mercedes-Benz', 'O500', 2016, 47000.0, 350.0, 'Onibus');
+INSERT INTO tb_onibus (veiculo_id, numero_de_assentos) VALUES (7, 45);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'RST9101', 'Marcopolo', 'G7', 2017, 48000.0, 380.0, 'Onibus');
+INSERT INTO tb_onibus (veiculo_id, numero_de_assentos) VALUES (8, 50);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'QWE2345', 'Scania', 'K360', 2018, 46000.0, 370.0, 'Onibus');
+INSERT INTO tb_onibus (veiculo_id, numero_de_assentos) VALUES (9, 48);
+
+INSERT INTO tb_veiculo (disponivel, placa, marca, modelo, ano_fabricacao, capacidade, custo_por_dia, categoria_veiculo) VALUES (TRUE, 'RTY6789', 'Comil', 'Campione 3.65', 2019, 49000.0, 400.0, 'Onibus');
+INSERT INTO tb_onibus (veiculo_id, numero_de_assentos) VALUES (10, 52);
