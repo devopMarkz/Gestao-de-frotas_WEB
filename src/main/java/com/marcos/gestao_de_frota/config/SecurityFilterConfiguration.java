04bead651a35a86e5611ebffc84e6b3994c91971
devopMarkz <marcosdev2002@gmail.com>
2025-01-11 18:47:35 -0300

    Criando implementação de UserDetailsService

--- src/main/java/com/marcos/gestao_de_frota/config/SecurityFilterConfiguration.java
+++ src/main/java/com/marcos/gestao_de_frota/config/SecurityFilterConfiguration.java
@@ -1,9 +1,27 @@
 package com.marcos.gestao_de_frota.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityFilterConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.POST, "/usuarios/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .build();
+    }
+
 }
