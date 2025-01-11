b2e15960d18beaba5ff77e5dceb4dd8e68e46a77
devopMarkz <marcosdev2002@gmail.com>
2025-01-11 18:24:10 -0300

    Adicionando Spring Security

---
+++ src/main/java/com/marcos/gestao_de_frota/config/SecurityFilterConfiguration.java
@@ -0,0 +1,9 @@
+package com.marcos.gestao_de_frota.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterConfiguration {
+}
