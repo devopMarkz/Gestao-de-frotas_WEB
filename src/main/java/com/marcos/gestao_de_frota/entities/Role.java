b18f6f6236e548d0da7b70c28047440f829a3d8f
devopMarkz <marcosdev2002@gmail.com>
2025-01-12 00:55:44 -0300

    Criando entidade Role

--- src/main/java/com/marcos/gestao_de_frota/entities/Role.java
+++ src/main/java/com/marcos/gestao_de_frota/entities/Role.java
@@ -1,4 +1,52 @@
 package com.marcos.gestao_de_frota.entities;
 
-public class Role {
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+
 }
